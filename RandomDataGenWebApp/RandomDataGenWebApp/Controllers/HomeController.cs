44a804b0f33cacae8e031ee3f71c57ecf3bee321
ljakhouski <aleksandr.ru.www@gmail.com>
2023-01-23 20:06:20 +0300

    all files added

---
+++ RandomDataGenWebApp/RandomDataGenWebApp/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomDataGenWebApp.Models;
+using System.Diagnostics;
+
+namespace RandomDataGenWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public int Generate(int seed, string country, int errVal)
+        {
+            int rd = new Random(seed).Next();
+            JsonSelector.Select();
+            return rd;
+            //return View();
+        }
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ RandomDataGenWebApp/RandomDataGenWebApp/JsonSelector.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System.Reflection;
+using System.Text.Json;
+
+namespace RandomDataGenWebApp
+{
+    public static class JsonSelector
+    {
+        public static void Select()
+        {
+            //var assembly = Assembly.GetExecutingAssembly();
+            //var resourceName = "EmbeddedResource.Data.foreign_names.json";
+
+           /* using (Stream stream = new FileStream(""))
+            using (StreamReader reader = new StreamReader(""))
+            {
+                var myDeserializedClass = JsonSerializer.DeserializeAsync<List<Root>>(stream);
+
+                var res = myDeserializedClass.Result;
+                //string jsonFile = reader.ReadToEnd(); //Make string equal to full file
+            }*/
+
+            using (StreamReader r = new StreamReader("foreign_names.json"))
+            {
+                string json = r.ReadToEnd();
+                List<Root> items = JsonConvert.DeserializeObject<List<Root>>(json);
+
+                var new_ = from item in items where item.origin.Contains("American") select item.name;
+            }
+
+
+        }
+    }
+
+    public class Root
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string meaning { get; set; }
+        public string gender { get; set; }
+        public string origin { get; set; }
+        public int PeoplesCount { get; set; }
+        public DateTime WhenPeoplesCount { get; set; }
+        public string Source { get; set; }
+    }
+}
